f4c42575de259610de34b99058954a9cc41be055
wangweijun1 <wangweijun1@le.com>
2018-01-24 17:13:32 +0800

    first commit

---
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksActivity.java
@@ -0,0 +1,10 @@
+package com.example.android.architecture.blueprints.todoapp.tasks;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wangweijun on 2018/1/24.
+ */
+
+public class TasksActivity extends AppCompatActivity{
+}
